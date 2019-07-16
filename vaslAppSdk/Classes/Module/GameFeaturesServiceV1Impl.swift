766e5215e7b041d06379b1d9353060a86427ac00
Srostami <Srostami2005@gmail.com>
2019-07-16 12:15:57 +0430

    clean up

--- vaslAppSdk/Classes/Module/GameFeaturesServiceV1Impl.swift
+++ vaslAppSdk/Classes/Module/GameFeaturesServiceV1Impl.swift
@@ -16,7 +16,7 @@ public class GameFeaturesServiceV1Impl  : GameFeaturesServiceV1 {
     }
     
     private func getFeatures(completion: @escaping (Com_Vasl_Vaslapp_Modules_Game_Global_Proto_Holder_GetFeatures?,String?) -> Void,force : Bool) {
-        var params = Dictionary<String,Any>()
+        let params = Dictionary<String,Any>()
         RestService.post(url: PublicValue.getUrlBase() + "/api/v1/game/features/get", params, completion: { (result, error) in
             do{
                 if let result = result {
